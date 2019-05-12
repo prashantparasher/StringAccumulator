d07598aafe2fbf3bd650b8a0ef33e7411ff0c3fc
prashantparasher <kietgene@gmail.com>
2019-05-12 18:39:30 +0530

    Update StringAccumulatorTest.java

--- src/test/java/com/accumulator/StringAccumulatorTest.java
+++ src/test/java/com/accumulator/StringAccumulatorTest.java
@@ -6,31 +6,33 @@ import static org.junit.Assert.assertEquals;
 
 public class StringAccumulatorTest {
 
+	//Test cases as per the sequence of requirements 
+	
 	@Test
 	public void TestEquals1() throws Exception {
 
-		String expectedEmpObj = "6";
-		assertEquals(expectedEmpObj, new StringAccumulator().addString("1,2,3"));
+		String expectedResult = "6";
+		assertEquals(expectedResult, new StringAccumulator().addString("1,2,3"));
 	}
 
 	@Test
 	public void TestEquals2() throws Exception {
 
-		String expectedEmpObj = "21";
-		assertEquals(expectedEmpObj, new StringAccumulator().addString("1,2,3\n4,5,6"));
+		String expectedResult = "21";
+		assertEquals(expectedResult, new StringAccumulator().addString("1,2,3\n4,5,6"));
 	}
 
 	@Test
 	public void TestEquals3() throws Exception {
-		String expectedEmpObj = "6";
-		assertEquals(expectedEmpObj, new StringAccumulator().addString("1\n2,3"));
+		String expectedResult = "6";
+		assertEquals(expectedResult, new StringAccumulator().addString("1\n2,3"));
 	}
 
 	@Test
 	public void TestEquals4() throws Exception {
-		String expectedEmpObj = "3";
+		String expectedResult = "3";
 		System.out.println(new StringAccumulator().addString("//;\n1;2"));
-		assertEquals(expectedEmpObj, new StringAccumulator().addString("//;\n1;2"));
+		assertEquals(expectedResult, new StringAccumulator().addString("//;\n1;2"));
 	}
 
 	@Test(expected = Exception.class)
@@ -41,30 +43,30 @@ public class StringAccumulatorTest {
 
 	@Test
 	public void TestEquals6() throws Exception {
-		String expectedEmpObj = "5";
+		String expectedResult = "5";
 
-		assertEquals(expectedEmpObj, new StringAccumulator().addString("//;\n1000;2;3"));
+		assertEquals(expectedResult, new StringAccumulator().addString("//;\n1000;2;3"));
 	}
 
 	@Test
 	public void TestEquals7() throws Exception {
-		String expectedEmpObj = "6";
+		String expectedResult = "6";
 
-		assertEquals(expectedEmpObj, new StringAccumulator().addString("//***\n1***2***3"));
+		assertEquals(expectedResult, new StringAccumulator().addString("//***\n1***2***3"));
 	}
 
 	@Test
 	public void TestEquals8() throws Exception {
-		String expectedEmpObj = "6";
+		String expectedResult = "6";
 
-		assertEquals(expectedEmpObj, new StringAccumulator().addString("//*|%\n1*2%3"));
+		assertEquals(expectedResult, new StringAccumulator().addString("//*|%\n1*2%3"));
 	}
 
 	@Test
 	public void TestEquals9() throws Exception {
-		String expectedEmpObj = "6";
+		String expectedResult = "6";
 
-		assertEquals(expectedEmpObj, new StringAccumulator().addString("//***|%%\n1***2%%3"));
+		assertEquals(expectedResult, new StringAccumulator().addString("//***|%%\n1***2%%3"));
 	}
 
 }
